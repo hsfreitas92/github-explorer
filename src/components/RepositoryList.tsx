e31f1c9e7228b7cd74cb95b50b44b28bc505e1e7
Henrique Silva de Freitas <69264667+hsfreitas92@users.noreply.github.com>
2021-12-12 15:35:35 -0300

    first commit

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,32 @@
+import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss'
+import { useEffect, useState } from "react";
+
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
+export function RepositoryList() {
+  const [repositories, setRespositories] = useState<Repository[]>([]);
+
+  useEffect(()=>{
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRespositories(data))
+  }, []);
+  
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
+        
+      </ul>
+    </section>
+  );
+}
